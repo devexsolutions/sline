a93bca844cb6d3106923714b09b80d5b63556faa
Jose Antonio Campos Segura <jacampossegura@gmail.com>
2021-10-21 21:49:00 +0200

    Elimanada tarifa duplicada

--- resources/views/tarifas.blade.php
+++ resources/views/tarifas.blade.php
@@ -28,25 +28,7 @@
 
 
         
-        <div class="pricing-plan border-t-4 border-solid border-white bg-white text-center max-w-lg	 mx-auto hover:border-indigo-600 transition-colors duration-300">
-                <div class="p-6 md:py-8">
-                    <h4 class="font-medium leading-tight text-2xl mb-2">Plan 48</h4>
-                    <p class="text-gray-600">48 Alineadores</p>
-                </div>
-                <div class="pricing-amount bg-indigo-100 p-6 transition-colors duration-300">
-                    <div class=""><span class="text-4xl font-semibold">1.300€</span></div>
-                </div>
-                <div class="p-6 text-left">
-                    <ul class="leading-loose list-disc">                    
-                    <li>*Alineadores Adicionales a 20€ / unidad</li>
-                    <li>Precio cerrado al paciente desde el primer diágnostico en clínica</li>
-                    <li>5 años para completar el tratamiento si finalmente no se consumiensen todos los alineadores contratados en el plan</li>
-                    <li>Estudio 150€. Incluido estudio de refinamiento intermedio o final.</li>
-                    <li>Estudio de refinamiento adicional 120€</li>
-                    <li>Primer envío gratuito. Envío de alineadores adicionales a 6€, excepto Canarias, Ceuta y Melilla.</li>
-                    </ul>                    
-                </div>
-        </div>
+        
 
      
  
